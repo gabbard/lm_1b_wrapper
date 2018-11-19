1f858b3ba4b0d861771b272360da6c7854c6b485
Ryan Gabbard <gabbard@isi.edu>
2018-11-19 14:17:09 -0800

    Add profiling support

--- scripts/dump_sentence_probs.py
+++ scripts/dump_sentence_probs.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
-import csv
-import time
+"""
+Applies the Google Billion word language model to a file of sentences.
 
+The input is expected to be a tab-separated values file with the sentence in the first field as
+a sequence of space-separated tokens.
+
+The output will be the same as the input except a language model probability of each sentence will
+be inserted as a new first field.  The remaining fields on each row will be the same as the input
+fields.
+
+Note that providing a list of sentences, one-per-line, as input is valid.
+
+If the parameter `profile` is set to `True`, profiling information will be dumped to a file in the
+working directory with the prefix `profile_`.
+"""
+import csv
+import os
+import time
+from contextlib import AbstractContextManager
+
+import tensorflow.contrib.tfprof
 from vistautils.parameters import Parameters
 from vistautils.parameters_only_entrypoint import parameters_only_entry_point
 
 from lm1b_wrapper.lm1b import LM1B
+
+
+class NoOpContextManager(AbstractContextManager):
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
 
 def main(params: Parameters):
     graph_def_file = params.existing_file("graph_def_file")
@@ -13,28 +37,40 @@ def main(params: Parameters):
     vocab_file = params.existing_file("vocab_file")
     sentences_file = params.existing_file("sentences_file")
     output_file = params.creatable_file("output_file")
+    do_profiling = params.optional_boolean_with_default("profile", False)
 
-    lm = LM1B.load(graph_def_file=graph_def_file,
-                   checkpoint_file=checkpoint_glob,
-                   vocab=vocab_file)
+    with tensorflow.contrib.tfprof.ProfileContext(
+            os.getcwd(),
+            trace_steps=range(2, 10),
+            dump_steps=range(1, 10, 2),
+            enabled=params.optional_boolean_with_default("profile", False)
+        ):
+        lm = LM1B.load(graph_def_file=graph_def_file,
+                       checkpoint_file=checkpoint_glob,
+                       vocab=vocab_file)
 
-    start_time = None
-    num_sentences_processed = 0
+        start_time = None
+        num_tokens_processed = 0
 
-    with open(sentences_file, 'r') as inp:
-        with open(output_file, 'w', newline='') as out:
-            csvwriter = csv.writer(out)
-            for line in inp:
-                if line:
-                    tokens = line.split(' ')
-                    csvwriter.writerow((tokens, lm.log_probability_of_sentence(tokens)))
-                    num_sentences_processed += 1
+        with open(sentences_file, 'r', newline='') as inp:
+            csv_input = csv.reader(inp, delimiter='\t')
+            with open(output_file, 'w', newline='') as out:
+                csv_output = csv.writer(out, delimiter='\t')
+                for line in csv_input:
+                    tokens = line[0].split(' ')
+                    output_row = list(line)
+                    output_row.insert(0, lm.log_probability_of_sentence(tokens))
+                    csv_output.writerow(output_row)
                     # we delay till after the first sentence to avoid counting startup time
-                    if num_sentences_processed == 1:
+                    if num_tokens_processed == 0:
                         start_time = time.time()
+                    num_tokens_processed += len(tokens)
 
-    print(f"Processed {num_sentences_processed - 1} sentences in {time.time() - start_time} "
-          f"seconds. First sentence not included in time calculation")
+    elapsed_time = time.time() - start_time
+    print(f"Processed {num_tokens_processed - 1} sentences in {elapsed_time} "
+          f"seconds, {num_tokens_processed / elapsed_time} tokens per second. First sentence not "
+          f"included in time calculation.")
+
 
 if __name__ == "__main__":
     parameters_only_entry_point(main)
--- src/lm1b_wrapper/lm1b.py
+++ src/lm1b_wrapper/lm1b.py
@@ -3,6 +3,7 @@ import math
 from pathlib import Path
 from typing import Any, Mapping, Union, Sequence
 
+import contexttimer
 import numpy as np
 import tensorflow as tf
 from attr import attrs, attrib
@@ -29,47 +30,50 @@ class LM1B:
     @staticmethod
     def load(*, graph_def_file: Path, checkpoint_file: Path,
              vocab: Union[Path, CharsVocabulary]) -> 'LM1B':
-        resolved_vocab: CharsVocabulary
-        if isinstance(vocab, CharsVocabulary):
-            resolved_vocab = vocab
-        else:
-            resolved_vocab = CharsVocabulary(str(vocab), _MAX_WORD_LEN)
+        with contexttimer.Timer() as loading_timer:
+            resolved_vocab: CharsVocabulary
+            if isinstance(vocab, CharsVocabulary):
+                resolved_vocab = vocab
+            else:
+                resolved_vocab = CharsVocabulary(str(vocab), _MAX_WORD_LEN)
 
-        ### copied from Tensorflow's model repo's lm_1b_eval.py
-        with tf.Graph().as_default():
-            logging.info('Recovering graph.')
-            with tf.gfile.FastGFile(str(graph_def_file), 'r') as f:
-                s = f.read()
-                gd = tf.GraphDef()
-                text_format.Merge(s, gd)
+            ### copied from Tensorflow's model repo's lm_1b_eval.py
+            with tf.Graph().as_default():
+                logging.info('Recovering graph.')
+                with tf.gfile.FastGFile(str(graph_def_file), 'r') as f:
+                    s = f.read()
+                    gd = tf.GraphDef()
+                    text_format.Merge(s, gd)
 
-            tf.logging.info('Recovering Graph %s', graph_def_file)
-            t = {}
-            [t['states_init'], t['lstm/lstm_0/control_dependency'],
-             t['lstm/lstm_1/control_dependency'], t['softmax_out'], t['class_ids_out'],
-             t['class_weights_out'], t['log_perplexity_out'], t['inputs_in'],
-             t['targets_in'], t['target_weights_in'], t['char_inputs_in'],
-             t['all_embs'], t['softmax_weights'], t['global_step']
-             ] = tf.import_graph_def(gd, {}, ['states_init',
-                                              'lstm/lstm_0/control_dependency:0',
-                                              'lstm/lstm_1/control_dependency:0',
-                                              'softmax_out:0',
-                                              'class_ids_out:0',
-                                              'class_weights_out:0',
-                                              'log_perplexity_out:0',
-                                              'inputs_in:0',
-                                              'targets_in:0',
-                                              'target_weights_in:0',
-                                              'char_inputs_in:0',
-                                              'all_embs_out:0',
-                                              'Reshape_3:0',
-                                              'global_step:0'], name='')
+                tf.logging.info('Recovering Graph %s', graph_def_file)
+                t = {}
+                [t['states_init'], t['lstm/lstm_0/control_dependency'],
+                 t['lstm/lstm_1/control_dependency'], t['softmax_out'], t['class_ids_out'],
+                 t['class_weights_out'], t['log_perplexity_out'], t['inputs_in'],
+                 t['targets_in'], t['target_weights_in'], t['char_inputs_in'],
+                 t['all_embs'], t['softmax_weights'], t['global_step']
+                 ] = tf.import_graph_def(gd, {}, ['states_init',
+                                                  'lstm/lstm_0/control_dependency:0',
+                                                  'lstm/lstm_1/control_dependency:0',
+                                                  'softmax_out:0',
+                                                  'class_ids_out:0',
+                                                  'class_weights_out:0',
+                                                  'log_perplexity_out:0',
+                                                  'inputs_in:0',
+                                                  'targets_in:0',
+                                                  'target_weights_in:0',
+                                                  'char_inputs_in:0',
+                                                  'all_embs_out:0',
+                                                  'Reshape_3:0',
+                                                  'global_step:0'], name='')
 
-            logging.info('Recovering checkpoint %s\n', checkpoint_file)
-            sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
-                                                    log_device_placement=True))
-            sess.run('save/restore_all', {'save/Const:0': str(checkpoint_file)})
-            sess.run(t['states_init'])
+                logging.info('Recovering checkpoint %s\n', checkpoint_file)
+                sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                                        log_device_placement=True))
+                sess.run('save/restore_all', {'save/Const:0': str(checkpoint_file)})
+                sess.run(t['states_init'])
+
+        logging.info("Loaded language model in %s seconds", loading_timer.elapsed)
 
         return LM1B(session=sess, name_to_node=t, vocab=resolved_vocab)
 
@@ -97,19 +101,20 @@ class LM1B:
         # of each of its tokens given the preceding context
         log_prob_sum = 0.0
         for token in tokens:
-            dist_over_next_words = self._session.run(
-                self._name_to_node['softmax_out'],
-                feed_dict={
-                    self._name_to_node['char_inputs_in']: char_ids_inputs,
-                    self._name_to_node['inputs_in']: inputs,
-                    self._name_to_node['targets_in']: targets,
-                    self._name_to_node['target_weights_in']: weights})
-            token_idx = self._vocab.word_to_id(token)
-            log_prob_sum += math.log(dist_over_next_words[0][token_idx])
+            with contexttimer.Timer() as token_timer:
+                dist_over_next_words = self._session.run(
+                    self._name_to_node['softmax_out'],
+                    feed_dict={
+                        self._name_to_node['char_inputs_in']: char_ids_inputs,
+                        self._name_to_node['inputs_in']: inputs,
+                        self._name_to_node['targets_in']: targets,
+                        self._name_to_node['target_weights_in']: weights})
+                token_idx = self._vocab.word_to_id(token)
+                log_prob_sum += math.log(dist_over_next_words[0][token_idx])
 
-            # prepare this word to be the context for the next word
-            inputs[0, 0] = token_idx
-            char_ids_inputs[0, 0, :] = self._vocab.word_to_char_ids(token)
+                # prepare this word to be the context for the next word
+                inputs[0, 0] = token_idx
+                char_ids_inputs[0, 0, :] = self._vocab.word_to_char_ids(token)
 
         # restore original state so that future calls to log_probability_of_sentence
         # are not affected by past calls
